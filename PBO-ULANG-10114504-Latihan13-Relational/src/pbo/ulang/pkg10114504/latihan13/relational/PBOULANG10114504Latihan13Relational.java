e4a77e3ad3014a447b03dd6112253e671937463f
Sandy Akbar <55833676+sandytwelve12@users.noreply.github.com>
2019-09-26 05:34:16 -0700

    Add files via upload

---
+++ PBO-ULANG-10114504-Latihan13-Relational/src/pbo/ulang/pkg10114504/latihan13/relational/PBOULANG10114504Latihan13Relational.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo.ulang.pkg10114504.latihan13.relational;
+
+/**
+ *
+ * @author 
+ * NAMA     : Sandy Akbar
+ * KELAS    : PBO-Ulang
+ * NIM      : 10114504
+ * DESKRIPSI: Program ini berisi program relasi
+ */
+public class PBOULANG10114504Latihan13Relational {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int bil1 = 10;
+        int bil2 = 20;
+        System.out.println("a == b " + (bil1 == bil2));
+        System.out.println("a != b " + (bil1 != bil2));
+        System.out.println("a > b " + (bil1 > bil2));
+        System.out.println("a < b " + (bil1 < bil2));
+        System.out.println("b >= b " + (bil1 >= bil2));
+        System.out.println("b <= b " + (bil1 <= bil2));
+    }
+    
+}
